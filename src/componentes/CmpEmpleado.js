cf94ca5e3abbe5b0b3df5f863654096ccc3f29e7
pmarquez29 <pablo.marquez@ucb.edu.bo>
2025-04-29 22:54:03 -0400

    subiendo gestor de empleados

---
+++ src/componentes/CmpEmpleado.js
@@ -0,0 +1,23 @@
+import React from "react";
+ import "../estilos/App.css";
+ function CmpEmpleado({ empleado, cambiarEstado }) {
+ return (
+    <div>
+        <div className={`empleado-card ${empleado.activo ? "activo" : "inactivo"}`}>
+            <p>
+                <strong>{empleado.apellido} {empleado.nombre}</strong>
+            </p>
+            <p>Cargo: {empleado.cargo}</p>
+            <p>Estado: <span className={empleado.activo ? "estado-activo" : "estado-inactivo"}>
+                {empleado.activo ? "Activo " : "Inactivo "}
+            </span></p>
+            <button className="cambiar-estado-btn" onClick={() =>
+                cambiarEstado(empleado.id)}>
+                Cambiar Estado
+            </button>
+        </div>
+    </div>
+    );
+    }
+    export default CmpEmpleado;
+   
